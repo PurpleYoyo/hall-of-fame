d5578bede3d99755bb197e6f7d42c79c7a79c4ee
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2026-04-28 16:32:49 -0400

    defaults

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function buildHofTable() {
     const hof_table = document.getElementById("hof-table");
     let entries = [];
 
-    let game;
+    let game = null;
     const game_radios = document.getElementsByName('game');
     for (let i = 0; i < game_radios.length; i++) {
         if (game_radios[i].checked) {
@@ -25,13 +25,21 @@ function buildHofTable() {
         }
     }
 
-    let run_type;
+    if (!game) {
+        game = 'little_emerald';
+    }
+
+    let run_type = null;
     const run_type_radios = document.getElementsByName('run_type');
     for (let i = 0; i < run_type_radios.length; i++) {
         if (run_type_radios[i].checked) {
             run_type = run_type_radios[i].value;
             break;
         }
+    }
+
+    if (!run_type) {
+        run_type = 'hardcore_nuzlocke';
     }
 
     data[game][run_type].forEach(entry => {
